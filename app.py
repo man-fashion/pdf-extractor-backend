d4b1cfa579fe9124050370195de136bcd9b8ba35
DhaivatV <vipatdhaivat25@gmail.com>
2024-03-16 17:00:45 +0530

    ban gaye sare apis

--- app.py
+++ app.py
@@ -1,12 +1,29 @@
 # flask basic boiler plate 
-
+from bson import ObjectId
 from flask import Flask, render_template, request, redirect, url_for, jsonify
 import os
 from flask_cors import CORS
 from utils.s3Operations import upload_file_to_s3, delete_file_from_s3, get_bucket_contents
+from parser_1 import process_pdf_data
+import requests
+from utils.storedata import store_data, group_addresses_by_pincode, group_similar_addresses
 
 app = Flask(__name__)
 CORS(app)
+
+def download_pdf(url, save_path="user_bill.pdf"):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        with open(save_path, 'wb') as f:
+            f.write(response.content)
+        print("PDF downloaded successfully")
+        return True
+    else:
+        print("Failed to download PDF. Status code:", response.status_code)
+        return False
+
 
 @app.route('/')
 def index():
@@ -21,6 +38,41 @@ def upload_file():
             output = upload_file_to_s3(file, bucket_name)
             
             return jsonify({'message': 'File uploaded successfully', 'url': output})
+
+# this route will take s3 url in request download the pdf and extract the details and will save it into the db
+@app.route('/get-details', methods=['POST'])
+def get_details():
+    if request.method == "POST":
+        data = request.json
+        url = data.get('url')
+        filename = "user_bill.pdf"
+        res = download_pdf(url)
+
+        if res==True:
+            data = process_pdf_data(filename)
+            for items in data:
+
+                try:
+                    store_data(items)
+                except Exception as e:
+                    print("An error occurred while storing data:", e)
+
+            os.remove(filename)
+            return jsonify({"response":True,'message': 'Data Added successfully'})
+        else:
+            return jsonify({"response":False, 'message': 'Failed to extract data'})
+        
+
+
+@app.route('/get-addresses-by-pincode', methods=['GET'])
+def get_addresses_by_pincode():
+    return jsonify(group_addresses_by_pincode())
+
+# create a route to get similar addresses
+@app.route('/get-similar-addresses', methods=['GET'])
+def get_similar_addresses():
+    return jsonify(group_similar_addresses())
+
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ utils/storedata.py
@@ -0,0 +1,89 @@
+from pymongo import MongoClient
+import jsonschema
+from bson import json_util
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import os
+
+MONGO_URI=os.environ.get("MONGO_URI")
+
+# MongoDB connection
+client = MongoClient(MONGO_URI)
+db = client['Man_Fashion_Invoice_Data']
+collection = db['invoice-data']
+
+# Schema definition
+schema = {
+    "type": "object",
+    "properties": {
+        "Name": {"type": "string"},
+        "address": {"type": "string"},
+        "city": {"type": "string"},
+        "pincode": {
+            "anyOf": [
+                {"type": "string", "pattern": "^\d{6}$"},
+                {"type": "string", "pattern": "^NA$"}
+            ]
+        },
+        "state": {"type": "string"}
+    },
+    "required": ["Name", "address", "city", "pincode", "state"]
+}
+
+def store_data(data):
+    try:
+        jsonschema.validate(data, schema)
+        collection.insert_one(data)
+    except jsonschema.exceptions.ValidationError as e:
+        print("Data validation failed. Error:", e)
+    except Exception as e:
+        print("An error occurred while storing data:", e)
+
+def get_address_vectors(addresses):
+    # Combine address, city, state, and pincode into a single string
+    combined_addresses = [' '.join([address['address'], address['city'], address['state'], address['pincode']]) for address in addresses]
+
+    # Vectorize the combined addresses
+    vectorizer = TfidfVectorizer()
+    address_vectors = vectorizer.fit_transform(combined_addresses)
+
+    return address_vectors
+
+# get all the data from db and store it into list. now in that list check where the pincode is same and then for all such adreses create a list map it with that particular pincode and create a dict
+
+def group_addresses_by_pincode():
+    print("function called")
+    data = list(collection.find())
+    pincode_map = {}
+    for address in data:
+        pincode = address.get('pincode')
+        if pincode:
+            if pincode not in pincode_map:
+                pincode_map[pincode] = []
+            pincode_map[pincode].append(address)
+
+    # Convert ObjectId to string representation for JSON serialization
+    pincode_map_serializable = {
+        pincode: json_util.dumps(addresses) for pincode, addresses in pincode_map.items()
+    }
+    return pincode_map_serializable
+
+
+# now similarly get adress address+city+state+pincode and check is if similar address exsists using vector similarity and if yes then create a mapping like {"01": [address1, address2, address3]}
+
+def group_similar_addresses(threshold=0.7):
+
+    addresses = list(collection.find())
+
+    address_vectors = get_address_vectors(addresses)
+    similarity_matrix = cosine_similarity(address_vectors)
+    grouped_addresses = {}
+
+    for i in range(len(addresses)):
+        similar_indices = [j for j, sim in enumerate(similarity_matrix[i]) if sim > threshold and j != i]
+        if similar_indices:
+            key = str(i).zfill(2)
+            grouped_addresses[key] = [json_util.dumps(addresses[i])] + [json_util.dumps(addresses[j]) for j in similar_indices]
+
+    return grouped_addresses
+
